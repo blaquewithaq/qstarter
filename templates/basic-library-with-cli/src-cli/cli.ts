d4341361ecd9adb33d289ac0665dcf12bb87455f
Blaque Allen <blaquewithaq@gmail.com>
2025-10-24 19:53:23 -0500

    chore: save future templates

--- templates/basic-library-with-cli/src-cli/cli.ts
+++ templates/basic-library-with-cli/src-cli/cli.ts
@@ -1,3 +1,7 @@
-import { hello } from "@lib";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const { hello } = await import(path.join(__dirname, "index.js"));
 
 console.log(hello());
